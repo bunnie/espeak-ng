a2e373ca82d16e69e35867c4b2d277a793cac449
bunnie <bunnie@kosagi.com>
2022-02-25 02:47:26 +0800

    working minimal test program

--- src/test.c
+++ src/test.c
@@ -24,6 +24,13 @@
 #include <synthesize.h>
 #include <synthdata.h>
 
+int samplerate;
+bool quiet = false;
+unsigned int samples_total = 0;
+unsigned int samples_split = 0;
+unsigned int samples_split_seconds = 0;
+unsigned int wavefile_count = 0;
+
 extern int saved_parameters[N_SPEECH_PARAM]; // Parameters saved on synthesis start
 const int ffi_param_defaults[N_SPEECH_PARAM] = {
 	0,   // silence (internal use)
@@ -42,6 +49,109 @@ const int ffi_param_defaults[N_SPEECH_PARAM] = {
 	0,   // line length
 	0,   // voice type
 };
+
+FILE *f_wavfile = NULL;
+void testWrite4Bytes(FILE *f, int value)
+{
+	// Write 4 bytes to a file, least significant first
+	int ix;
+
+	for (ix = 0; ix < 4; ix++) {
+		fputc(value & 0xff, f);
+		value = value >> 8;
+	}
+}
+
+int OpenWavFile(char *path, int rate)
+{
+	static unsigned char wave_hdr[44] = {
+		'R', 'I', 'F', 'F', 0x24, 0xf0, 0xff, 0x7f, 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ',
+		0x10, 0, 0, 0, 1, 0, 1, 0,  9, 0x3d, 0, 0, 0x12, 0x7a, 0, 0,
+		2, 0, 0x10, 0, 'd', 'a', 't', 'a',  0x00, 0xf0, 0xff, 0x7f
+	};
+
+	if (path == NULL)
+		return 2;
+
+	while (isspace(*path)) path++;
+
+	f_wavfile = NULL;
+	if (path[0] != 0) {
+		if (strcmp(path, "stdout") == 0) {
+#ifdef PLATFORM_WINDOWS
+			// prevent Windows adding 0x0d before 0x0a bytes
+			_setmode(_fileno(stdout), _O_BINARY);
+#endif
+			f_wavfile = stdout;
+		} else
+			f_wavfile = fopen(path, "wb");
+	}
+
+	if (f_wavfile == NULL) {
+		fprintf(stderr, "Can't write to: '%s'\n", path);
+		return 1;
+	}
+
+	fwrite(wave_hdr, 1, 24, f_wavfile);
+	testWrite4Bytes(f_wavfile, rate);
+	testWrite4Bytes(f_wavfile, rate * 2);
+	fwrite(&wave_hdr[32], 1, 12, f_wavfile);
+	return 0;
+}
+
+void CloseWavFile(void)
+{
+	unsigned int pos;
+
+	if ((f_wavfile == NULL) || (f_wavfile == stdout))
+		return;
+
+	fflush(f_wavfile);
+	pos = ftell(f_wavfile);
+
+	if (fseek(f_wavfile, 4, SEEK_SET) != -1)
+		testWrite4Bytes(f_wavfile, pos - 8);
+
+	if (fseek(f_wavfile, 40, SEEK_SET) != -1)
+		testWrite4Bytes(f_wavfile, pos - 44);
+
+	fclose(f_wavfile);
+	f_wavfile = NULL;
+}
+
+
+static int SynthCallback(short *wav, int numsamples, espeak_EVENT *events)
+{
+	while (events->type != 0) {
+		if (events->type == espeakEVENT_SAMPLERATE) {
+			samplerate = events->id.number;
+			samples_split = samples_split_seconds * samplerate;
+		} else if (events->type == espeakEVENT_SENTENCE) {
+			// start a new WAV file when the limit is reached, at this sentence boundary
+			if ((samples_split > 0) && (samples_total > samples_split)) {
+				CloseWavFile();
+				samples_total = 0;
+				wavefile_count++;
+			}
+		}
+		events++;
+	}
+
+	if (f_wavfile == NULL) {
+		if (samples_split > 0) {
+			if (OpenWavFile("test1.wav", samplerate) != 0)
+				return 1;
+		} else if (OpenWavFile("test1.wav", samplerate) != 0)
+			return 1;
+	}
+
+	if (numsamples > 0) {
+		samples_total += numsamples;
+		fwrite(wav, numsamples*2, 1, f_wavfile);
+	}
+	return 0;
+}
+
 
 void dump_param_stack(void) {
     int i, j;
@@ -88,6 +198,7 @@ int main(int argc, char **argv) {
 	WavegenInit(srate, 0);
 	LoadConfig();
 
+    // setup parameters
 	memset(&current_voice_selected, 0, sizeof(current_voice_selected));
 	SetVoiceStack(NULL, "");
 	SynthesizeInit();
@@ -104,9 +215,7 @@ int main(int argc, char **argv) {
 	SetParameter(espeakPUNCTUATION, option_punctuation, 0);
 	SetParameter(espeakWORDGAP, 0, 0);
 
-#ifdef USE_ASYNC
-	fifo_init();
-#endif
+	// fifo_init();
 
 	option_phonemes = 0;
 	option_phoneme_events = 0;
@@ -118,10 +227,18 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-    result = espeak_ng_InitializeOutput(0x0002, 0, NULL); // PLAYBACK_MODE -> ENOUTPUT_MODE_SPEAK_AUDIO -> 0x0002
-    int samplerate = espeak_ng_GetSampleRate();
+    // setup output stream
+    result = espeak_ng_InitializeOutput(0x0001, 0, NULL);
+    // PLAYBACK_MODE -> ENOUTPUT_MODE_SPEAK_AUDIO -> 0x0002
+    // FILE -> ENOUTPUT_MODE_SYNCHRONOUS -> 0x0001
+    samplerate = espeak_ng_GetSampleRate();
     printf("sample rate: %d\n", samplerate);
+    samples_split = 0;
+    samples_split_seconds = 30 * 60;
+    samples_split = samplerate * samples_split_seconds;
+    espeak_SetSynthCallback(SynthCallback);
 
+    /// setup voices
     char voicename[40];
     strcpy(voicename, ESPEAKNG_DEFAULT_VOICE);
     printf("default voice: %s\n", voicename);
@@ -144,11 +261,6 @@ int main(int argc, char **argv) {
         DoVoiceChange(voice);
         SetVoiceStack(&voice_select, "");
 	}
-	int phoneme_options = 0;
-	int phonemes_separator = 0;
-	FILE *f_phonemes_out = stdout;
-
-	espeak_SetPhonemeTrace(phoneme_options | (phonemes_separator << 8), f_phonemes_out);
 
     char *p_text = "The quick brown fox jumps over the lazy dogs";
     int size;
@@ -157,4 +269,8 @@ int main(int argc, char **argv) {
     espeak_Synth(p_text, size+1, 0, POS_CHARACTER, 0, synth_flags, NULL, NULL);
 
     printf("goodbye world!\n");
+	espeak_ng_Synchronize();
+	CloseWavFile();
+	espeak_ng_Terminate();
+	return 0;
 }
